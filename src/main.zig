770d7ff25a500e71afcde43a883b321905e23c07
Dracco-10 <draccoovando@gmail.com>
2026-08-26 12:20:14 -0600

    Poligono 3 completo

--- src/main.zig
+++ src/main.zig
@@ -8,16 +8,9 @@ const Point = struct {
     y: f32,
 };
 const polygon1 = [_]Point{
-    .{ .x = 165, .y = 380 },
-    .{ .x = 185, .y = 360 },
-    .{ .x = 188, .y = 330 },
-    .{ .x = 207, .y = 345 },
-    .{ .x = 233, .y = 330 },
-    .{ .x = 230, .y = 360 },
-    .{ .x = 250, .y = 380 },
-    .{ .x = 220, .y = 385 },
-    .{ .x = 205, .y = 410 },
-    .{ .x = 193, .y = 383 },
+    .{ .x = 377, .y = 249 },
+    .{ .x = 411, .y = 197 },
+    .{ .x = 436, .y = 249 },
 };
 
 fn findIntersections(allocator: std.mem.Allocator, polygon: []const Point, y: f32) ![]f32 {
@@ -84,7 +77,7 @@ pub fn main(init: std.process.Init) !void {
                     @intFromFloat(y),
                     @intFromFloat(x_end),
                     @intFromFloat(y),
-                    rl.YELLOW,
+                    rl.RED,
                 );
             }
         }
